1fcb7049a01d6cf19573d136891e2cf7bd64c14c
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-11-12 20:31:39 -0400

    add default test

--- src/users/user.controller.spec.ts
+++ src/users/user.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from '../common/common.module';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule, CommonModule],
       controllers: [UserController],
       providers: [UserService],
     }).compile();
--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from '../common/common.module';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
@@ -6,6 +8,7 @@ describe('UserService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule, CommonModule],
       providers: [UserService],
     }).compile();
 
