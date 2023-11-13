04959fdf0ce9d678d74477ed72cc94485eb55e87
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-11-13 17:40:12 -0400

    return to default rabbitmq port

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -1,6 +1,6 @@
 // Mapping of environment variables in case they do not come
 export const EnvConfiguration = () => ({
-  rabbitmqPort: process.env.RABBITMQ_PORT || '5673',
+  rabbitmqPort: process.env.RABBITMQ_PORT || '5672',
   rabbitmqEndpoint: process.env.RABBITMQ_ENDPOINT || 'amqp://localhost',
 
   remoteApiUrl:
