5429568aa817d33c1a5abb74be64e150b7daad4a
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-11-12 21:41:06 -0400

    test getUsers method from user.service

---
+++ __mock__/user.mock.ts
@@ -0,0 +1,41 @@
+export const usersMock = [
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me',
+    phone: '586.493.6943 x140',
+    website: 'jacynthe.com',
+    company: {
+      name: 'Abernathy Group',
+      catchPhrase: 'Implemented secondary concept',
+      bs: 'e-enable extensible e-tailers',
+    },
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io',
+    phone: '(775)976-6794 x41206',
+    website: 'conrad.com',
+    company: {
+      name: 'Yost and Sons',
+      catchPhrase: 'Switchable contextually-based project',
+      bs: 'aggregate real-time technologies',
+    },
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz',
+    phone: '024-648-3804',
+    website: 'ambrose.net',
+    company: {
+      name: 'Hoeger LLC',
+      catchPhrase: 'Centralized empowering task-force',
+      bs: 'target end-to-end models',
+    },
+  },
+];
--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -2,20 +2,70 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from '../common/common.module';
 import { UserService } from './user.service';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
+import { RabbitMqAdapter } from '../common/adapters/rabbitmq.adapter';
+import { usersMock } from '../../__mock__/user.mock';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: jest.Mocked<AxiosAdapter>;
+  let broker: jest.Mocked<RabbitMqAdapter>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule, CommonModule],
-      providers: [UserService],
+      imports: [ConfigModule],
+      providers: [
+        UserService,
+        {
+          provide: AxiosAdapter,
+          useFactory: () => ({
+            get: jest.fn(),
+          }),
+        },
+        {
+          provide: RabbitMqAdapter,
+          useFactory: () => ({
+            connect: jest.fn(),
+            createChannel: jest.fn(),
+            assertExchange: jest.fn(),
+            assertQueue: jest.fn(),
+            bindToQueue: jest.fn(),
+            publish: jest.fn(),
+          }),
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    httpMock = module.get(AxiosAdapter);
+    broker = module.get(RabbitMqAdapter);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('#getUsers()', () => {
+    beforeEach(() => {
+      httpMock.get.mockResolvedValue(usersMock);
+    });
+
+    it('should call the get method of the HTTP service', async () => {
+      await service.getUsers();
+      expect(httpMock.get).toHaveBeenCalled();
+    });
+
+    it('should fetch users', async () => {
+      const users = await service.getUsers();
+
+      expect(users.length).toBeGreaterThan(0);
+      expect(users.length).toEqual(usersMock.length);
+    });
+
+    it('should be sorted in descending order by id', async () => {
+      const users = await service.getUsers();
+
+      expect(users[0].id).toEqual(usersMock[2].id);
+    });
   });
 });
